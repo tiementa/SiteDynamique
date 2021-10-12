d3d0ab3170cf000c2b34b5e6afe91a381f60e2ef
Tiementa Hamadou <tiementa.hamadou@odc.local>
2021-10-12 16:20:59 +0000

    notre tp site vitrine

--- accueil.php
+++ accueil.php
@@ -1,12 +1,13 @@
-<?php 
+<?php
 include './bd.php';
 
-        $req = $conn->query('SELECT * FROM personnages');
+$req = $conn->query('SELECT * FROM personnages');
 ?>
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
-<meta charset="utf-8">
+  <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
   <title>Squadfree Bootstrap Template - Index</title>
@@ -14,15 +15,16 @@ include './bd.php';
   <meta content="" name="keywords">
   <?php include './link.php'; ?>
 </head>
+
 <body>
-<?php include './header.php'; ?>
+  <?php include './header.php'; ?>
 
   <!-- ======= Hero Section ======= -->
   <section id="hero">
     <div class="hero-container" data-aos="fade-up">
-      <h1> Bienvenue dans le monde du numerique </h1>
+      <h2> Bienvenue dans le monde du numerique </h2>
       <h2> Le numérique est notre avenir. Que ce soit dans le domaine de l'économie, de l'emploi ou de l'éducation, le numérique est un facteur indispensable à l'évolution, au progrès et à la pérennité de notre société. </h2>
-      <a href="#about" class="btn-get-started scrollto"><i class="bx bx-chevrons-down"></i></a>
+      <a href="#team" class="btn-get-started scrollto"><i class="bx bx-chevrons-down"></i></a>
     </div>
   </section><!-- End Hero -->
 
@@ -40,27 +42,27 @@ include './bd.php';
         </div>
 
         <div class="row">
-            <?php
-                while($data = $req->fetch()){
-            ?>
+          <?php
+          while ($data = $req->fetch()) {
+          ?>
 
-          <div class="col-lg-4 col-md-6">
-            <div class="member" data-aos="fade-up">
-              <div class="pic">
-                <a href="detailPersonnage.php?id=<?= $data['id']?>">
-                  <img src="./upload/<?= $data['nom_image']?>"  class="img-fluid" alt="" style="width:300px;height:300px;">
-                </a>
+            <div class="col-lg-4 col-md-6">
+              <div class="member" data-aos="fade-up">
+                <div class="pic">
+                  <a href="detailPersonnage.php?id=<?= $data['id'] ?>">
+                    <img src="./upload/<?= $data['nom_image'] ?>" class="img-fluid" alt="" style="width:300px;height:300px;">
+                  </a>
                 </div>
-              <div class="member-info">
-                <h4><?= $data['nom_complet'] ?></h4>
-                <span><?= $data['titre'] ?></span>
-               
+                <div class="member-info">
+                  <h4><?= $data['nom_complet'] ?></h4>
+                  <span><?= $data['titre'] ?></span>
+
+                </div>
               </div>
             </div>
-          </div>
-          
+
           <?php
-            } 
+          }
           ?>
 
         </div>
@@ -90,19 +92,18 @@ include './bd.php';
 
 
 
-<?php
-     while($data = $req->fetch()){
-?>
+  <?php
+  while ($data = $req->fetch()) {
+  ?>
 
-        <h2><?= $data['nom_complet'] ?></h2>
-        <a href="detailPersonnage.php?id=<?= $data['id']?>"> 
-            <img src="./upload/<?= $data['nom_image']?>" width="300px"><br>
-        </a>
-<?php
-} 
-?>
+    <h2><?= $data['nom_complet'] ?></h2>
+    <a href="detailPersonnage.php?id=<?= $data['id'] ?>">
+      <img src="./upload/<?= $data['nom_image'] ?>" width="300px"><br>
+    </a>
+  <?php
+  }
+  ?>
 
 </body>
+
 </html>
-
-
